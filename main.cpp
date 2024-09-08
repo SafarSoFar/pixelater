4386d5e7a2bcb8f567e012a765e7be0f62ab9f53
SafarSoFar <so_far_away0525@proton.me>
2024-09-08 16:36:51 +0500

    added canvas texture usage

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+void UpdateTexture() {}
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -20,6 +22,18 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "Pixel Editor");
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+  Color pixelColors[screenWidth * screenHeight];
+
+  for (int i = 0; i < screenWidth; i++) {
+    for (int j = 0; j < screenHeight; j++) {
+      pixelColors[i * screenHeight + j] = WHITE;
+    }
+  }
+
+  Image image = {pixelColors, screenWidth, screenHeight, 1,
+                 PIXELFORMAT_UNCOMPRESSED_R8G8B8A8};
+  Texture2D screenTexture = LoadTextureFromImage(image);
 
   rlImGuiSetup(true);
 
@@ -38,22 +52,32 @@ int main(void) {
     ClearBackground(RAYWHITE);
     BeginDrawing();
     rlImGuiBegin();
-
+    ImGui::SetWindowPos(ImVec2(0.0f, 0.0f));
     ImGui::SliderInt("Brush Size", &brushSize, 1, 10);
 
     if (IsKeyPressed(KEY_Q)) {
       ClearBackground(RAYWHITE);
     }
+    DrawTexture(screenTexture, 0, 0, RAYWHITE);
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      /*Vector2 mousePos = GetMousePosition();*/
+      /*for (int i = mousePos.x - brushSize; i <= mousePos.x + brushSize; i++)
+       * {*/
+      /*  for (int j = mousePos.y - brushSize; j <= mousePos.y + brushSize; j++)
+       * {*/
+      /*    if (i >= 0 && i < screenWidth && j >= 0 && j < screenHeight) {*/
+      /*      pixelColors[i * screenHeight + j] = BLACK;*/
+      /*      UpdateTexture(screenTexture, pixelColors);*/
+      /*    }*/
+      /*  }*/
+      /*}*/
       Vector2 mousePos = GetMousePosition();
-      for (int i = mousePos.x - brushSize; i <= mousePos.x + brushSize; i++) {
-        for (int j = mousePos.y - brushSize; j <= mousePos.y + brushSize; j++) {
-          if (i >= 0 && i < screenWidth && j >= 0 && j < screenHeight) {
-            DrawPixel(i, j, BLACK);
-          }
-        }
-      }
+      pixelColors[(int)mousePos.x + (int)mousePos.y * screenWidth] = BLACK;
+
+      UpdateTexture(screenTexture, &pixelColors);
+      /*pixelColors[(int)mousePos.x * screenHeight + (int)mousePos.y] = BLACK;*/
+      /*UpdateTexture(screenTexture, pixelColors);*/
     }
 
     rlImGuiEnd();
