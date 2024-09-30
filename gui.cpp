63a83e6596e31b68c3bad4c0d44bbaedf99f3c3b
SafarSoFar <so_far_away0525@proton.me>
2024-09-30 13:05:27 +0500

    added raylib source to compile full project out of the box

---
+++ gui.cpp
@@ -0,0 +1,5 @@
+/*#include "gui.h"*/
+/**/
+/*void DrawAndControlGUI(){*/
+/**/
+/*}*/
--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@
 #include <raylib.h>
 #include <raymath.h>
 
-#define STB_IMAGE_IMPLEMENTATION
+/*#define STB_IMAGE_IMPLEMENTATION*/
 #include "stb/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+/*#define STB_IMAGE_WRITE_IMPLEMENTATION*/
 #include "stb/stb_image_write.h"
 
 using std::vector;
